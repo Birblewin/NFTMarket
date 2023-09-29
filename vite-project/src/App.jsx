b9991735f31ab6e844503970a78f356ddc4e24eb
Brian Kinyua <briankinyua0101@gmail.com>
2023-09-29 17:22:03 +0300

    Design the carousel component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Navbar from './components/navbar'
+import Carousel from './components/carousel/Carousel'
 import './App.css'
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <>
-        <Navbar/>
+        <Carousel/>
     </>
   )
 }
---
+++ vite-project/src/components/carousel/Carousel.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cards from './Cards'
+
+const Carousel = () => {
+  return (
+    <div className='carousel-container-outer'>
+        <Cards />
+    </div>
+  )
+}
+
+export default Carousel
