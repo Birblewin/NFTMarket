6e3de4619d6ba68e94e8883769dbf39972da09da
Brian Kinyua <briankinyua0101@gmail.com>
2023-09-29 17:21:22 +0300

    Create NFT details

---
+++ vite-project/src/components/carousel/NFTDetails.jsx
@@ -0,0 +1,38 @@
+import image1 from "./images/image1.png";
+import image2 from "./images/image2.jpeg";
+import image3 from "./images/image3.png";
+import image4 from "./images/image4.jpeg";
+import image5 from "./images/image5.jpeg";
+const Details = [
+  {
+    cardImage: image1,
+    cardTitle: "Apprentices",
+    cardInfo:
+      "Enter the dazzling world of APPRENTICES: an anime PFP collection that's not just about avatars—it's an epic narrative-driven universe. Uniting cool aesthetics with community passion, it's where your chosen character becomes a hero in a captivating saga. Dive into this thrilling adventure where creativity and beauty collide. Join now and let your anime avatar's story shine!",
+  },
+  {
+    cardImage: image2,
+    cardTitle: "EarthNodes",
+    cardInfo:
+      "EarthNodes are the brain powering the Sharing Economy of World Mobile, the first mobile network owned by the people and built on blockchain. Connecting the unconnected narrative-driven universe. Uniting cool aesthetics with community passion, it's where your chosen character becomes a hero in a captivating saga. Dive into this thrilling adventure where creativity and beauty collide. Join now and let your anime avatar's story shine!",
+  },
+  {
+    cardImage: image3,
+    cardTitle: "The Ape Society",
+    cardInfo:
+      "The Ape Society is a collection of 7,000 NFTs generated on the Cardano blockchain. Every holder is a member of one of 35 families, and gets access to the DAO. We aim to create tools, resources, art, and harness the power of community to create a breeding ground for knowledge, collaboration, and fun.",
+  },{
+    cardImage: image4,
+    cardTitle: "Apprentices",
+    cardInfo:
+      "Enter the dazzling world of APPRENTICES: an anime PFP collection that's not just about avatars—it's an epic narrative-driven universe. Uniting cool aesthetics with community passion, it's where your chosen character becomes a hero in a captivating saga. Dive into this thrilling adventure where creativity and beauty collide. Join now and let your anime avatar's story shine!",
+  },{
+    cardImage: image5,
+    cardTitle: "EarthNodes",
+    cardInfo:
+      "EarthNodes are the brain powering the Sharing Economy of World Mobile, the first mobile network owned by the people and built on blockchain. Connecting the unconnected narrative-driven universe. Uniting cool aesthetics with community passion, it's where your chosen character becomes a hero in a captivating saga. Dive into this thrilling adventure where creativity and beauty collide. Join now and let your anime avatar's story shine!",
+  },
+];
+
+
+export default Details
