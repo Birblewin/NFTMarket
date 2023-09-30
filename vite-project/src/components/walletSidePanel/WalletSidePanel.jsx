040e38c8e788e8533dabc8a22d5c9c932f264cc2
itsbluejelly <ronniedenzel0@gmail.com>
2023-10-01 00:56:11 +0300

    fleshed out the walletsidepanel component

--- vite-project/src/components/walletSidePanel/WalletSidePanel.jsx
+++ vite-project/src/components/walletSidePanel/WalletSidePanel.jsx
@@ -10,10 +10,15 @@ export default function WalletSidePanel(){
         <div className="side_panel_container">
             <div className="side_panel_container__side_panel">
                 <figure className="side_panel_container__side_panel--title">
-
+                    <p>&#128091;</p>
+                    <h1>Connect Wallet</h1>
+                    
+                    <figcaption className='side_panel--title--description'>
+                        <p>By connecting your wallet, you agree to the <span>Terms & Conditions</span> and <span>Privacy Policy</span></p>
+                    </figcaption>
                 </figure>
 
-                <small className="side_panel_container__side_panel--heading"></small>
+                <small className="side_panel_container__side_panel--heading">Select a wallet to install it</small>
 
                 <div className="side_panel_container__side_panel--wallets_grid"></div>
             </div>
