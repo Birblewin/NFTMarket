b80440b690828cb69dcf382b051db34e16e4ec8d
itsbluejelly <ronniedenzel0@gmail.com>
2023-10-02 00:24:31 +0300

    created CarouselEntry skeleton

--- vite-project/src/pages/launchpad/launchesPage/CarouselEntry.jsx
+++ vite-project/src/pages/launchpad/launchesPage/CarouselEntry.jsx
@@ -1,4 +1,21 @@
 // EXPORTING DEFAULT CAROUSELENTRY FUNCTION
 export default function CarouselEntry(){
-    return(<p>Hi</p>)
+    return(
+        <div className="carousel_entry_container">
+            <img src="" alt="" />
+            
+            <div className="carousel_entry_container__description">
+                <h1 className="carousel_entry_container__description--title"></h1>
+                <span className="carousel_entry_container__description--banner"></span>
+                <p className="carousel_entry_container__description--paragraph"></p>
+
+                <div>
+                    <div className="carousel_entry_container__description--buttons">
+                        <span className="description--buttons--calender"></span>
+                        <button className="description--buttons--link"></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
