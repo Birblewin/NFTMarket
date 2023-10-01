977d78c15650069e750b6238f897e18a0de32e5b
itsbluejelly <ronniedenzel0@gmail.com>
2023-10-02 02:17:13 +0300

    fleshed out and implemented carousel component

--- vite-project/src/pages/launchpad/launchesPage/LaunchesCarousel.jsx
+++ vite-project/src/pages/launchpad/launchesPage/LaunchesCarousel.jsx
@@ -1,9 +1,23 @@
-// IMPORING NECESSARY MODULES
+// IMPORING NECESSARY MODULES AND COMPONENTS AND DATA
+import React from "react"
 import Details from "../../../components/carousel/NFTDetails"
 import CarouselEntry from "./CarouselEntry"
 
 // EXPORTING THE LAUNCHESCAROUSEL FUNCTIONS
 export default function LaunchesCarousel(){
+    // A STATE TO HOLD THE CURRENT INDEX OF CARD DISPLAYED
+    const [currentCard, setCurrentCard] = React.useState(0)
+
+    // A FUNCTION TO MOVE TO THE NEXT CARD
+    function checkNextCard(){
+        setCurrentCard(prevCard => prevCard < Details.length - 1 ? prevCard + 1 : 0)
+    }
+
+    // A FUNCTION TO MOVE TO THE PREVIOUS CARD
+    function checkPreviousCard(){
+        setCurrentCard(prevCard => prevCard > 0 ? prevCard - 1 : Details.length - 1)
+    }
+
     // AN ARRAY OF CAROUSELENTRIES
     const generatedEntriesArray = Details.map(
         detail => (<CarouselEntry 
@@ -15,8 +29,18 @@ export default function LaunchesCarousel(){
     )
 
     return(
-        <div>
-            {generatedEntriesArray}
+        <div className="carousel-container">
+            <button 
+                className="carousel-container__decrease-button"
+                onClick={() => checkPreviousCard()}
+            >&lt;</button>
+
+            <div className="carousel-container__current-slide">{generatedEntriesArray[currentCard]}</div>
+
+            <button 
+                className="carousel-container__increase-button"
+                onClick={() => checkNextCard()}
+            >&gt;</button>
         </div>
     )
 }
