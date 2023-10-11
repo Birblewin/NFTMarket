74205c32e37cd6f26dab30457e411a22f76be25f
itsbluejelly <ronniedenzel0@gmail.com>
2023-10-12 01:25:07 +0300

    Merge pull request #22 from Birblewin/Ronnie
    added view of small screen in navbar, below 640px

--- vite-project/src/components/navbar/Navbar.jsx
+++ vite-project/src/components/navbar/Navbar.jsx
@@ -5,8 +5,8 @@ import { NavLink } from 'react-router-dom';
 //  IMPORING NECESSARY COMPONENTS
 import './Navbar.css'; // Import your CSS file for styling
 import SearchIcon from '@mui/icons-material/Search';
-import LightModeIcon from '@mui/icons-material/LightMode';
-import NotificationsIcon from '@mui/icons-material/Notifications';
+// import LightModeIcon from '@mui/icons-material/LightMode';
+// import NotificationsIcon from '@mui/icons-material/Notifications';
 
 // EXPORTING A FUNCTION THAT CREATES A NAVBAR COMPONENT
 export default function Navbar(props) {
@@ -18,22 +18,55 @@ export default function Navbar(props) {
           <p>Birble.store</p>
         </div>
       </NavLink>
-      
-      <SearchIcon className='search-icon'/>
 
-      <div>
+      {/* <div className='search-bar-container'>
+        <SearchIcon className='search-icon'/>
+
         <input
           className="search-bar"
           type="text"
           placeholder="Search collection, or NFT"
           name="searchInput"
-          // eslint-disable-next-line react/prop-types
-         /*  value={props.searchData.searchInput || "Search collection, or NFT"}
-          onChange={props.handleSearchData} */
         />
+      </div> */}
+
+      <div className='small-screen-buttons'>
+        <button 
+          className='small-screen-buttons__search-button'
+          title='search-button'
+        >
+          <SearchIcon 
+            className='search-icon'
+            onClick={props.showSearchBar}
+          />
+        </button>
+
+        <button 
+          className='small-screen-buttons__wallet-button'
+          onClick={props.showWalletSidePanel}
+          title='show-wallet'
+        >
+          <img 
+            crossOrigin="anonymous" 
+            id="" 
+            src="https://static.jpgstoreapis.com/icons/wallet-outline-dark.svg" 
+            alt="wallet icon"
+            loading='lazy'
+          />
+        </button>
+        
+        <button 
+          className='small-screen-buttons__side-panel-button'
+          title='show-sidebar'
+          onClick={props.showLinkSidePanel}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </button>
       </div>
      
-      <div className="nav-links">
+      {/* <div className="nav-links">
         <div>
           <NavLink className='rewards' to="/rewards">Rewards</NavLink>
         </div>
@@ -44,10 +77,10 @@ export default function Navbar(props) {
           
           <div className="dropdown-content">
             {/* Dropdown links */}
-            <NavLink to="/allcollections">All collections</NavLink>
-            <NavLink to="/allnfts">All NFTs</NavLink>
+            {/* <NavLink to="/allcollections">All collections</NavLink>
+            <NavLink to="/allnfts">All NFTs</NavLink> */}
             {/* Add more links as needed */}
-          </div>
+          {/* </div>
         </span>
       </div>
 
@@ -56,11 +89,11 @@ export default function Navbar(props) {
           <a >Creators</a>
           <div className="dropdown-content">
             {/* Dropdown links */}
-            <NavLink href="#">Create your collection</NavLink>
-            <NavLink href="#">Verify your collection</NavLink>
+            {/* <NavLink href="#">Create your collection</NavLink>
+            <NavLink href="#">Verify your collection</NavLink> */}
             {/* Add more links as needed */}
-          </div>
-        </span>
+          {/* </div> */}
+        {/* </span>
       </div>
 
       <div className='dropdown'>
@@ -68,10 +101,10 @@ export default function Navbar(props) {
           <a >About</a>
           <div className="dropdown-content">
             {/* Dropdown links */}
-            <NavLink to="#">Our team</NavLink>
-            <NavLink to="#">Our impact</NavLink>
+            {/* <NavLink to="#">Our team</NavLink>
+            <NavLink to="#">Our impact</NavLink> */}
             {/* Add more links as needed */}
-          </div>
+          {/* </div>
         </span>
       </div>
 
@@ -80,10 +113,10 @@ export default function Navbar(props) {
           <a >Launchpad</a>
           <div className="dropdown-content">
             {/* Dropdown links */}
-            <NavLink to="/launchpad/launches">Launches</NavLink>
-            <NavLink to="#">Apply For Launchpad</NavLink>
+            {/* <NavLink to="/launchpad/launches">Launches</NavLink>
+            <NavLink to="#">Apply For Launchpad</NavLink> */}
             {/* Add more links as needed */}
-          </div>
+          {/* </div>
         </span>
       </div>
 
@@ -100,8 +133,8 @@ export default function Navbar(props) {
       <button 
         className='wallet'
         onClick={props.showWalletSidePanel}
-      >Connect wallet</button>
-    </div>  
+      >Connect wallet</button> */}
+    {/* </div>   */}
   </div>
   );
 }
