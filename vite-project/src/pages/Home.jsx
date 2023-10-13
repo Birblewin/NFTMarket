3731ad0846567ef84694a976d2d8f21edbc2dfbc
Brian Kinyua <briankinyua0101@gmail.com>
2023-10-13 12:24:27 +0300

    fix the table

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,17 +1,16 @@
-
-import Carousel from '../components/carousel/Carousel'
-import WhaleActivityContainer from '../components/whaleActivity/WhaleActivityContainer'
-import Table from '../components/Table/table'
-import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown'
-import Card from '../components/CollectionCard'
-import Cards from './AllCollections/Cards'
-import './Home.css'
+import Carousel from "../components/carousel/Carousel";
+import WhaleActivityContainer from "../components/whaleActivity/WhaleActivityContainer";
+import Table from "../components/Table/table";
+import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
+import Card from "../components/CollectionCard";
+import Cards from "./AllCollections/Cards";
+import "./Home.css";
 function Home() {
   return (
     <div>
-      <Carousel/>
-        <WhaleActivityContainer />
-        <Table/>
+      <Carousel />
+      <WhaleActivityContainer />
+      <Table />
       <div className="top-div">
         <div className="collection">
           <h3 className="title">Top collections </h3>
@@ -19,14 +18,18 @@ function Home() {
             <h3>Generative</h3>
             <KeyboardArrowDown className="arrow-down" />
           </div>
-     <button style={{cursor: "pointer"}} onClick={() => location.assign('/allcollections')}>View all</button>
         </div>
-        
-      
-          <section className="main-content">
+        <button
+          style={{ cursor: "pointer" }}
+          onClick={() => location.assign("/allcollections")}
+        >
+          View all
+        </button>
+      </div>
+
+      <section className="main-content">
         <Cards />
       </section>
-
     </div>
   );
 }
