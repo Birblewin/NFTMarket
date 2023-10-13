26af95705917ab844cb1b4fa376b93aa29877001
Brian Kinyua <briankinyua0101@gmail.com>
2023-10-13 17:04:21 +0300

    merge

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,29 +1,37 @@
-import Carousel from '../components/carousel/Carousel'
-import WhaleActivityContainer from '../components/whaleActivity/WhaleActivityContainer'
-import Table from '../components/Table/table'
-import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown'
-import Cards from './AllCollections/Cards'
-import './Home.css'
+import Carousel from "../components/carousel/Carousel";
+import WhaleActivityContainer from "../components/whaleActivity/WhaleActivityContainer";
+import Table from "../components/Table/table";
+import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
+import Cards from "./AllCollections/Cards";
+import "./Home.css";
 function Home() {
   return (
     <div>
-      <Carousel/>
-        <WhaleActivityContainer />
-        <Table/>
-        
-        <div className='top'>
-          <div className='collection'>
-            <h3 className='title'>Top collections  <span>Generative</span></h3> <KeyboardArrowDown className='arrow-down'/>
-          </div>
-          
-          <button style={{cursor: "pointer"}} onClick={() => location.assign('/allcollections')}>View all</button>
+      <Carousel />
+      <WhaleActivityContainer />
+      <Table />
+
+      <div className="top">
+        <div className="collection">
+          <h3 className="title">
+            Top collections <span>Generative</span>
+          </h3>{" "}
+          <KeyboardArrowDown className="arrow-down" />
         </div>
-        
-       
-<Cards/>
+
+        <button
+          style={{ cursor: "pointer" }}
+          onClick={() => location.assign("/allcollections")}
+        >
+          View all
+        </button>
+      </div>
+
+      <div className="main-content">
+        <Cards />
+      </div>
     </div>
-    
   );
 }
 
-export default Home
+export default Home;
