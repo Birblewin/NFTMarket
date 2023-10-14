31a95512c537553b3a2a7c6941e180048ca7de2c
itsbluejelly <ronniedenzel0@gmail.com>
2023-10-14 08:03:18 +0300

    cleaned up unnecessary code

--- vite-project/src/pages/launchpad/Launchpadpage/columns/overview-team.jsx
+++ vite-project/src/pages/launchpad/Launchpadpage/columns/overview-team.jsx
@@ -9,16 +9,6 @@ const Notes = () => {
   
     <div className='Details'>
     <Columnlayout>
-    <div className='Column-container'>
-      <div className="column">
-        <p>This is the left column.</p>
-        <p>It can contain paragraphs or other content.</p>
-      </div>
-      <div className="column">
-        <p>This is the right column.</p>
-        <p>It can also contain paragraphs or other content.</p>
-      </div>
-     </div>
     </Columnlayout>
     </div>
   </div>
