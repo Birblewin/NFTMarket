2045039f71c3343defb4510829a4f22a1eef9f1f
itsbluejelly <ronniedenzel0@gmail.com>
2023-10-14 09:51:50 +0300

    replaced useState with global context

--- vite-project/src/layouts/RootLayout.jsx
+++ vite-project/src/layouts/RootLayout.jsx
@@ -10,15 +10,26 @@ import WalletSidePanel from '../components/walletSidePanel/WalletSidePanel'
 import LinkSidePanel from '../components/linkSidePanel/LinkSidePanel'
 import SearchBar from "../components/searchBar/SearchBar";
 
+// IMPORTING NECESSARY HOOKS
+import WalletSidePanelContextHook from "../hooks/WalletSidePanelContextHook";
+
 // IMPORT THE CSS FILE
 import './RootLayout.css'
 
 // EXPORTING A ROOTLAYOUT FUNCTION
 export default function RootLayout(){
+    // A BOOLEAN TO KEEP TRACK OF SHOWING THE SEARCHBAR
+    const [showSearchBar, setShowSearchBar] = React.useState(false)
+    // A BOOLEAN TO KEEP TRACK OF SHOWING THE SEARCHBAR
+    const [showLinkSidePanel, setShowLinkSidePanel] = React.useState(false)
+
     // A USESTATE FUNCTION TO TRACK CHANGES IN SEARCHBAR CONTENT
     const [formData, setFormData] = React.useState({
         search: ""
     })
+
+    // OBTAINING VARIOUS CONTEXTS FROM HOOKS
+    const {showWalletPanel, dispatch} = WalletSidePanelContextHook()
 
     // A FUNCTION TO UPDATE FORMDATA
     function updateFormData(e){
@@ -29,13 +40,6 @@ export default function RootLayout(){
             [name]: value
         }))
     }
-
-    // A BOOLEAN TO KEEP TRACK OF SHOWING THE WALLETSIDEPANNEL
-    const [showWalletSidePanel, setShowWalletSidePanel] = React.useState(false)
-    // A BOOLEAN TO KEEP TRACK OF SHOWING THE SEARCHBAR
-    const [showSearchBar, setShowSearchBar] = React.useState(false)
-    // A BOOLEAN TO KEEP TRACK OF SHOWING THE SEARCHBAR
-    const [showLinkSidePanel, setShowLinkSidePanel] = React.useState(false)
 
     return(
         <main>
@@ -52,11 +56,11 @@ export default function RootLayout(){
             }
 
             {
-                showWalletSidePanel && <WalletSidePanel hideWalletSidePanel = {() => setShowWalletSidePanel(false)}/>
+                showWalletPanel && <WalletSidePanel hideWalletSidePanel = {() => dispatch({ type: "HIDE_WALLET_PANEL" })}/>
             }
 
             <Navbar 
-                showWalletSidePanel = {() => setShowWalletSidePanel(true)}
+                showWalletSidePanel = {() => dispatch({ type: "SHOW_WALLET_PANEL" })}
                 showLinkSidePanel = {() => setShowLinkSidePanel(true)}
                 showSearchBar = {() => setShowSearchBar(true)}
             />
