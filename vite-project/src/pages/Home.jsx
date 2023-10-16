cbdc38daf68c9b1130be23026d3e5d1222e4d848
Brian Kinyua <briankinyua0101@gmail.com>
2023-10-16 18:14:39 +0300

    D-done

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -11,23 +11,24 @@ function Home() {
       <WhaleActivityContainer />
       <Table />
 
-      <div className="top">
-        <div className="collection">
-          <h3 className="title">
-            Top collections <span>Generative</span>
-          </h3>{" "}
-          <KeyboardArrowDown className="arrow-down" />
-        </div>
-
-        <button
-          style={{ cursor: "pointer" }}
-          onClick={() => location.assign("/allcollections")}
-        >
-          View all
-        </button>
-      </div>
-
       <div className="main-content">
+        <div className="top">
+          <div className="collection">
+            <h3 className="title">Top collections</h3>
+            <div className="generative">
+              <span className="h3">Generative</span>
+              <KeyboardArrowDown className="arrow-down" />
+            </div>
+          </div>
+
+          <button
+            className="generative-btn"
+            style={{ cursor: "pointer" }}
+            onClick={() => location.assign("/allcollections")}
+          >
+            View all
+          </button>
+        </div>
         <Cards />
       </div>
     </div>
