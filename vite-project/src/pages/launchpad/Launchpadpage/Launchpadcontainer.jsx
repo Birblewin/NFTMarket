1ab996af9a8b9d204577b37f7a27ddc1fb5f6b00
itsbluejelly <ronniedenzel0@gmail.com>
2023-10-17 06:45:44 +0300

    finished responsiveness of launchpadcontainer

--- vite-project/src/pages/launchpad/Launchpadpage/Launchpadcontainer.jsx
+++ vite-project/src/pages/launchpad/Launchpadpage/Launchpadcontainer.jsx
@@ -148,6 +148,102 @@ export default function Launchpadcontainer(){
           </div>
         </div>
       </div>
+
+      <div className="extra_large_screen_launchpad-container_entry">
+        <div className="container">
+          {/* LEFT SIDE OF CONTAINER */}
+          <div className="container__left-container">
+            {/* TITLE OF THE COLLECTION */}
+            <h1>{correctData.cardTitle}</h1>
+           
+            {/* WHERE 2 OF THE SELLING STATS ARE */}
+            <div className="container__selling_stats">
+              <div className='container__selling_stats--chosen_chain'></div>
+              <div className='container__selling_stats--total_sold'>Total items: {correctData.volume || 6969}</div>
+            </div>
+
+            {/* WHERE THE IMAGES ARE */}
+            <div className="container__image-container">
+              <img 
+                src={`${correctData.cardImage}`} 
+                alt="card-image"
+                loading='lazy'
+                width={100}
+                height={100}
+                title='card-image' 
+              />
+
+              <div className="container__image-container--bottom_images">
+                <img 
+                  src={`${correctData.cardImage}`} 
+                  alt="card-image"
+                  loading='lazy'
+                  width={100}
+                  height={100}
+                  title='card-image' 
+                />
+
+                <img 
+                  src={`${correctData.cardImage}`} 
+                  alt="card-image"
+                  loading='lazy'
+                  width={100}
+                  height={100}
+                  title='card-image' 
+                />
+
+                <img 
+                  src={`${correctData.cardImage}`} 
+                  alt="card-image"
+                  loading='lazy'
+                  width={100}
+                  height={100}
+                  title='card-image' 
+                />
+
+                <img 
+                  src={`${correctData.cardImage}`} 
+                  alt="card-image"
+                  loading='lazy'
+                  width={100}
+                  height={100}
+                  title='card-image' 
+                />
+              </div>
+            </div>
+          </div>
+
+          {/* RIGHT SIDE OF CONTAINER */}
+          <div className="container__right-container">
+            {/* THE REMAINING SELLING STAT */}
+            <div className='container__selling_stats--timing'>{dateDistance}</div>
+
+            {/* WHERE THE CONTENT IS */}
+            <div className="container__content-container">
+              <p>{correctData.cardInfo}</p>
+
+              <div className="container__content-container--paying_stats">
+                <h2>Price</h2>
+
+                <figure className="content-container--paying-stats--image">
+                  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" color="#e42575">
+                    <path d="M20.59 13.91L13.42 21.08C13.2343 21.266 13.0137 21.4135 12.7709 21.5141C12.5281 21.6148 12.2678 21.6666 12.005 21.6666C11.7422 21.6666 11.4819 21.6148 11.2391 21.5141C10.9963 21.4135 10.7757 21.266 10.59 21.08L2 12.5V2.5H12L20.59 11.09C20.9625 11.4647 21.1716 11.9716 21.1716 12.5C21.1716 13.0284 20.9625 13.5353 20.59 13.91V13.91Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+                    
+                    <path d="M7 7.5H7.01" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+                  </svg>
+
+                  <figcaption>
+                    <span>--</span>
+                    <b>◎</b>
+                  </figcaption>
+                </figure>
+
+                <button onClick={() => dispatch({ type: "SHOW_WALLET_PANEL" })}>Connect Wallet</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
