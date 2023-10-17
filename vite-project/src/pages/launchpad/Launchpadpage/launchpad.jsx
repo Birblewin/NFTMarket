a5d33ac0ad4467ee9a9524eee0b369a16a591507
itsbluejelly <ronniedenzel0@gmail.com>
2023-10-17 21:50:24 +0300

    updated launchpadpage files

--- vite-project/src/pages/launchpad/Launchpadpage/launchpad.jsx
+++ vite-project/src/pages/launchpad/Launchpadpage/launchpad.jsx
@@ -7,17 +7,6 @@ const Launchpad = () => {
     // A BOOLEAN TO KEEP TRACK OF WHETHER OR NOT TO VIEW THE TEAM SECTION
     const [viewTeam, setViewTeam] = React.useState(false)
 
-    // A FUNCTION TO GENERATE THE OVERVIEW SECTION
-    function OverviewGenerator() {
-        const OverviewArray = []
-
-        for(let i = 0; i < 1; i++){
-            OverviewArray.push(<Overview/>)
-        }
-
-        return OverviewArray
-    }
-
     // A FUNCTION TO GENERATE THE TEAM SECTION
     function TeamGenerator() {
         const TeamArray = []
@@ -30,53 +19,50 @@ const Launchpad = () => {
     }
 
   return (
-    <div className='Launchpad-container'>
+    <div className='launchpad_container'>
       <Launchpadcontainer/>
       
-      <div className="launches_page_container__viewOverviewToggle">
-                <p 
-                    onClick={() => setViewTeam(false)}
+      <div className="launchpad_container__viewOverviewToggle">
+            <p 
+                onClick={() => setViewTeam(false)}
+                
+                style={
                     
-                    style={
-                        
-                      viewTeam
-                            ? 
-                        null 
-                            : 
-                        {
-                            "backgroundColor": "#EE9E26",
-                            "border": "2px solid black",
-                            "borderRadius": "20px",
-                            "padding": "10px"
-                        }
+                    viewTeam
+                        ? 
+                    null 
+                        : 
+                    {
+                        "backgroundColor": "#EE9E26",
+                        "border": "2px solid black",
+                        "borderRadius": "20px",
+                        "padding": "10px"
                     }
-                >Overview</p>
- 
-                <p 
-                    onClick={() => setViewTeam(true)}
-                    
-                    style={
-                        viewTeam 
-                            ? 
-                        {
-                            "backgroundColor": "#EE9E26",
-                            "border": "2px solid black",
-                            "borderRadius": "20px",
-                            "padding": "10px"
-                        } 
-                            : 
-                        null
-                    }
-                >Team
-                </p>
-       
+                }
+            >Overview</p>
 
-            </div>
-                  
-            <div className={viewTeam ? "launchpad-page-container-Holder" : ""}>
-                {viewTeam ? TeamGenerator() : 
-                OverviewGenerator()}
-            </div>
+            <p 
+                onClick={() => setViewTeam(true)}
+                
+                style={
+                    viewTeam 
+                        ? 
+                    {
+                        "backgroundColor": "#EE9E26",
+                        "border": "2px solid black",
+                        "borderRadius": "20px",
+                        "padding": "10px"
+                    } 
+                        : 
+                    null
+                }
+            >Team</p>
+        </div>
+                
+        <div className={viewTeam ? "launchpad_container__teamCollectionsHolder" : ""}>
+            {viewTeam ? TeamGenerator() : 
+            <Overview/>}
+        </div>
     </div>
   )
 }
