529192502e8e629140961b81fd4885bf4e48606b
itsbluejelly <ronniedenzel0@gmail.com>
2023-11-08 23:45:59 +0300

    built for production

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import wasm from 'vite-plugin-wasm';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [react(),
-     wasm()
-],
+  plugins: [react(), wasm()],
+
+  build: {
+    target: 'esnext'
+  }
 })
